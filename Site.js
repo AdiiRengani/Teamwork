411c79aa48f609eb8e221a6687d924aafb4476c3
KendraRid9 <u16224940@up.ac.za>
2019-02-07 10:58:39 +0200

    my favourite commit

--- Site.js
+++ Site.js
@@ -1,1 +1,3 @@
- 
+function myFunction() {
+  alert("Im excited for INF354 because I would love to know how I can finally use the skills I have aquired and produce soemthing to release to the public.");
+}
